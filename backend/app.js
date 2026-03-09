239e774adb2c853f653b954b35ac7a1e09aba7cb
raees-khan03 <sraeeskhan03@gmail.com>
2026-03-10 02:13:02 +0500

    fix: all import paths corrected

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,12 @@ import cors from "cors";
 import "dotenv/config";
 import serverless from "serverless-http";
 
-import connectDb from "../config/mongodb.js";
+import connectDb from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
-import userRouter from "../routes/userRouter.js";
-import productRouter from "../routes/ProductRouter.js";
-import cartRouter from "../routes/cartRouter.js";
-import orderRouter from "../routes/orderRouter.js";
+import userRouter from "./routes/userRouter.js";
+import productRouter from "./routes/ProductRouter.js";
+import cartRouter from "./routes/cartRouter.js";
+import orderRouter from "./routes/orderRouter.js";
 
 const app = express();
 
@@ -27,5 +27,5 @@ app.get("/", (req, res) => {
   res.send("API is working on Vercel!");
 });
 
-// Export serverless handler
 export const handler = serverless(app);
+export default app;
